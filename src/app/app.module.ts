02a40e6d9282026072419e91a02c5734b53d0ad8
Nicholas Kwiecinski <nicholas.kwiecinski@gmail.com>
2018-05-26 18:48:43 -0500

    setup login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,41 +3,44 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AngularFireModule } from 'angularfire2';
 
 import { MyApp } from './app.component';
+import { LoginPage } from '../pages/login/login';
 
 
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDSV-SSg-AbPsGSLPtTLLKg5LsTwQVWb48",
+  authDomain: "get-it-done-d2aca.firebaseapp.com",
+  databaseURL: "https://get-it-done-d2aca.firebaseio.com",
+  projectId: "get-it-done-d2aca",
+  storageBucket: "get-it-done-d2aca.appspot.com",
+  messagingSenderId: "1013357034106"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-
+    LoginPage,
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp({
-      config: {
-      apiKey: "AIzaSyDSV-SSg-AbPsGSLPtTLLKg5LsTwQVWb48",
-      authDomain: "get-it-done-d2aca.firebaseapp.com",
-      databaseURL: "https://get-it-done-d2aca.firebaseio.com",
-      projectId: "get-it-done-d2aca",
-      storageBucket: "get-it-done-d2aca.appspot.com",
-      messagingSenderId: "1013357034106"
-    }
-    })
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-
+    LoginPage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
+    
   ]
 })
 export class AppModule {}
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { User } from '../../models/user';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 /**
  * Generated class for the LoginPage page.
@@ -18,11 +19,32 @@ export class LoginPage {
 
   user = {} as User
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private afAuth: AngularFireAuth,
+    public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  async loginUser(user: User)
+  {
+    try{
+     const result = this.afAuth.auth.signInWithEmailAndPassword(this.user.email, this.user.password);
+     console.log(result);
+
+     if(result) {
+     this.navCtrl.push('HomePage');
+     }
+
+    }catch (e) {
+      console.error(e);
+    }
+  }
+
+  registerUser()
+  {
+    this.navCtrl.push('RegisterPage');
   }
 
 }
